7354a5b30642e752baacdc7c4d44379e3a701221
RayeesaBanu <150254678+RayeesaBanu@users.noreply.github.com>
2024-07-30 07:22:31 +0000

    Datatypes

---
+++ 01_BASICS/02_Datatypes.js
@@ -0,0 +1,22 @@
+//primitive Datatypes
+
+let name="raeesa";
+let age=24;
+let isLoggedIn=true;
+let state;
+//let city=null;
+
+//number=>2 to power 53
+//bigInt
+//string => "raeesa"
+//boolean => true/false
+//Null => standalone value
+//Undefined
+//Symbol => Unique
+
+//NonPrimitive datatypes
+
+//object
+console.log(typeof("raeesa"));
+console.log(typeof(null));
+console.log(typeof(undefined));
---
+++ 01_BASICS/03_DatatypeConversion.js
@@ -0,0 +1,47 @@
+// let score="33abc";
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof(score));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let score=null;
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof(score));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// let score=undefined;
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof(score));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+//notes
+//"33" => 33
+//"33abc" => NaN
+//true => 1,false => 0
+
+// let isLoggedIn=1;
+// let isLoggedIn1=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn1);
+// console.log(booleanIsLoggedIn);
+
+//1 => true,0 => false
+//"" => false
+//"raeesa" => true
+
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
